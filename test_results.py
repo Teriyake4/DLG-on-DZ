37153d3db47e7e6e9808026f265a07ffacc36da5
Ian Chao <teriyake4@outlook.com>
2025-08-03 21:32:36 -0700

    Adjusted for updated csv file

--- test_results.py
+++ test_results.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import os
-import numpy as np
 
-results_path = os.path.join('.', "results/iDLG_lenet_MNIST_5e-06/results.csv")
-df = pd.read_csv(results_path, header=None)
+resultsPath = os.path.join('.', "results/baseline/")
+foResults = os.path.join(resultsPath, "fo/results.csv")
+zoResults = os.path.join(resultsPath, "zo/results.csv")
 
-# df = df.iloc[0:200, :] # use only 200 samples
 thresholds = [0.01, 0.005, 0.001, 0.0005, 0.0001]
-for threshold in thresholds:
-    print(f"MSE < {threshold}:", len(df[df.iloc[:, 2] < threshold]) / len(df)) # MSE threshold
 
-loss = df.iloc[:, 1].mean()
-mse = df.iloc[:, 2].mean()
+def calculateStats(df):
+    # for threshold in thresholds:
+    #     print(f"MSE < {threshold}:", len(df[df.iloc[:, 2] < threshold]) / len(df)) # MSE threshold
 
-lossm = df.iloc[:, 1].median()
-msem = df.iloc[:, 2].median()
+    print("DLG:")
+    print(f"Mean Loss: {df.iloc[:, 1].mean():.4e}, Mean MSE: {df.iloc[:, 2].mean():.4e}")
+    print(f"Median Loss: {df.iloc[:, 1].median():.4e}, Median MSE: {df.iloc[:, 2].median():.4e}")
+    print(f"Minimum MSE: {df.iloc[:, 2].min()}, Index: {df.iloc[:, 2].idxmin()}")
 
-min_index = df.iloc[:, 2].idxmin()
+    print("iDLG:")
+    print(f"Mean Loss: {df.iloc[:, 3].mean():.4e}, Mean MSE: {df.iloc[:, 4].mean():.4e}")
+    print(f"Median Loss: {df.iloc[:, 3].median():.4e}, Median MSE: {df.iloc[:, 4].median():.4e}")
+    print(f"Minimum MSE: {df.iloc[:, 3].min()}, Index: {df.iloc[:, 3].idxmin()}")
 
-print(f"Mean Loss: {loss:.4e}, Mean MSE: {mse:.4e}")
-print(f"Median Loss: {lossm:.4e}, Median MSE: {msem:.4e}")
-print(f"Minimum MSE: {df.iloc[min_index, 2]}, Index: {df.iloc[min_index, 0]}")
+fodf = pd.read_csv(foResults)
+zodf = pd.read_csv(zoResults)
+
+print("FO Results:")
+calculateStats(fodf)
+
+print('\n')
+
+print("ZO Results:")
+calculateStats(zodf)
