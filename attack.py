547501878360d08c7504fe55a249ce4be2034565
Ian Chao <teriyake4@outlook.com>
2025-08-06 13:01:49 -0700

    Adapted to main

--- attack.py
+++ attack.py
@@ -39,6 +39,8 @@ def main(mArgs, rArgs):
     lr = 1
     use_cuda = torch.cuda.is_available()
     device = f'cuda:{mArgs.gpus[0]}' if use_cuda else 'cpu'
+    device = "cpu"
+    print(device)
 
     tt = transforms.Compose([transforms.ToTensor()])
     tp = transforms.Compose([transforms.ToPILImage()])
@@ -90,19 +92,17 @@ def main(mArgs, rArgs):
                     gt_data = torch.cat((gt_data, tmp_datum), dim=0)
                     gt_label = torch.cat((gt_label, tmp_label), dim=0)
 
-            if mArgs.zero:
-                params_dict = {
-                    name: p for name, p in net.named_parameters() if p.requires_grad
-                }
-                mask_dict = {
-                    name: p for name, p in net.named_buffers() if 'mask' in name
-                }
-                dy_dx = opt.cge(opt.f, params_dict, mask_dict, mArgs.zoo_step_size, net, gt_data, gt_label, F.cross_entropy)
-            else:
-                # compute original gradient
-                out = net(gt_data)
-                y = criterion(out, gt_label)
-                dy_dx = torch.autograd.grad(y, net.parameters())
+            params_dict = {
+                name: p for name, p in net.named_parameters() if p.requires_grad
+            }
+            mask_dict = {
+                name: p for name, p in net.named_buffers() if 'mask' in name
+            }
+            dy_dx = opt.cge(opt.f, params_dict, mask_dict, mArgs.zoo_step_size, net, gt_data, gt_label, F.cross_entropy)
+            # compute original gradient
+            out = net(gt_data)
+            y = criterion(out, gt_label)
+            vanilla_dy_dx = torch.autograd.grad(y, net.parameters())
             
 
             zo_dy_dx = [grad.detach().clone() for grad in dy_dx]
@@ -126,44 +126,42 @@ def main(mArgs, rArgs):
             mses = []
             train_iters = []
             
-            vanilla_dy_dx = torch.autograd.grad(y, net.parameters())
             print('lr =', lr)
-            for alpha in alpha_grid:
-                zo_dy_dx_nudge = [zo_dy_dx[i] + (vanilla_dy_dx[i] - zo_dy_dx[i]) * alpha for i in range(len(zo_dy_dx))]
-                zo_dy_dx_nudge = [grad.detach().clone() for grad in zo_dy_dx_nudge]
-                for iteration in range(rArgs.num_iterations):
-                    closure = closures.BaseClosure(optimizer, net, criterion, method, dummy_data, dummy_label, label_pred, zo_dy_dx_nudge)
+            zo_dy_dx_nudge = [zo_dy_dx[i] + (vanilla_dy_dx[i] - zo_dy_dx[i]) * mArgs.alpha for i in range(len(zo_dy_dx))]
+            zo_dy_dx_nudge = [grad.detach().clone() for grad in zo_dy_dx_nudge]
+            for iteration in range(rArgs.num_iterations):
+                closure = closures.BaseClosure(optimizer, net, criterion, method, dummy_data, dummy_label, label_pred, zo_dy_dx_nudge)
 
-                    optimizer.step(closure)
-                    current_loss = closure().item()
-                    train_iters.append(iteration)
-                    losses.append(current_loss)
-                    mses.append(torch.mean((dummy_data - gt_data) ** 2).item())
+                optimizer.step(closure)
+                current_loss = closure().item()
+                train_iters.append(iteration)
+                losses.append(current_loss)
+                mses.append(torch.mean((dummy_data - gt_data) ** 2).item())
 
-                    if iteration % rArgs.printFreq == 0:
-                        current_time = str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
-                        print(current_time, iteration, 'loss = %.8f, mse = %.8f' % (current_loss, mses[-1]))
-                        history.append([tp(dummy_data[imidx].cpu()) for imidx in range(rArgs.num_dummy)])
-                        history_iters.append(iteration)
+                if iteration % rArgs.printFreq == 0:
+                    current_time = str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
+                    print(current_time, iteration, 'loss = %.8f, mse = %.8f' % (current_loss, mses[-1]))
+                    history.append([tp(dummy_data[imidx].cpu()) for imidx in range(rArgs.num_dummy)])
+                    history_iters.append(iteration)
 
-                        for imidx in range(rArgs.num_dummy):
-                            plt.figure(figsize=(12, 8))
-                            plt.subplot(3, 10, 1)
-                            plt.imshow(tp(gt_data[imidx].cpu()))
-                            for i in range(min(len(history), 29)):
-                                plt.subplot(3, 10, i + 2)
-                                plt.imshow(history[i][imidx])
-                                plt.title('iter=%d' % (history_iters[i]))
-                                plt.axis('off')
-                            if method == 'DLG':
-                                plt.savefig('%s/DLG_on_%s_%05d.png' % (rArgs.resultPath, imidx_list, imidx_list[imidx]))
-                                plt.close()
-                            elif method == 'iDLG':
-                                plt.savefig('%s/iDLG_on_%s_%05d.png' % (rArgs.resultPath, imidx_list, imidx_list[imidx]))
-                                plt.close()
+                    for imidx in range(rArgs.num_dummy):
+                        plt.figure(figsize=(12, 8))
+                        plt.subplot(3, 10, 1)
+                        plt.imshow(tp(gt_data[imidx].cpu()))
+                        for i in range(min(len(history), 29)):
+                            plt.subplot(3, 10, i + 2)
+                            plt.imshow(history[i][imidx])
+                            plt.title('iter=%d' % (history_iters[i]))
+                            plt.axis('off')
+                        if method == 'DLG':
+                            plt.savefig('%s/DLG_on_%s_%05d.png' % (rArgs.resultPath, imidx_list, imidx_list[imidx]))
+                            plt.close()
+                        elif method == 'iDLG':
+                            plt.savefig('%s/iDLG_on_%s_%05d.png' % (rArgs.resultPath, imidx_list, imidx_list[imidx]))
+                            plt.close()
 
-                        if current_loss < 0.000001:  # converge
-                            break
+                    if current_loss < 0.000001:  # converge
+                        break
 
             if method == 'DLG':
                 loss_DLG = losses
