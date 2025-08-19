e5182763bb9ae04279b74677c9a2f8846af2d583
Ian Chao <teriyake4@outlook.com>
2025-08-18 20:34:48 -0700

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+python experiments/sparse_gradient_training.py \
+  --network lenet \
+  --dataset MNIST \
+  --score layer_wise_random \
+  --sparsity 0.9 \
+  --sparsity-ckpt zo_grasp_0.9 \
+  --gpus 0 \
+  --lr 0.1 \
+  --master-port 29500
